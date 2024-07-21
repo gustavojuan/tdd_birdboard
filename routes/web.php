431f639f3fc26dfcf3e63b1ad233346a60792d16
gustavojuan <gustavojuan@gmail.com>
2024-07-21 08:04:43 +0200

    feat: add project creation feature wiht TDD

--- routes/web.php
+++ routes/web.php
@@ -1,7 +1,28 @@
 <?php
 
+use App\Models\Project;
 use Illuminate\Support\Facades\Route;
 
 Route::get('/', function () {
+
     return view('welcome');
+
+});
+
+Route::get('projects', function () {
+    $projects = Project::all();
+
+    return view('projects.index', compact('projects'));
+});
+
+
+
+
+Route::post('projects', function () {
+    //validate
+
+    //persist
+    Project::create(request(['title', 'description']));
+
+    //redirect
 });
---
+++ tests/Feature/ProjectsTest.php
@@ -0,0 +1,32 @@
+<?php
+
+namespace Tests\Feature;
+
+use Illuminate\Foundation\Testing\RefreshDatabase;
+use Illuminate\Foundation\Testing\WithFaker;
+use Tests\TestCase;
+
+class ProjectsTest extends TestCase
+{
+
+    use WithFaker, RefreshDatabase;
+
+    /** @test */
+    public function a_user_can_create_a_project(): void
+    {
+
+        $this->withoutExceptionHandling();
+
+        $attributes = [
+            'title' => $this->faker->title,
+            'description' => $this->faker->text,
+        ];
+
+        $this->post('projects', $attributes);
+
+        $this->assertDatabaseHas('projects', $attributes);
+
+        $this->get('/projects')->assertSee($attributes['title']);
+
+    }
+}
